3f8984b6becd672029eea8f11373be609e0a23c9
leson238 <leson238@gmail.com>
2020-06-21 19:15:15 -0400

    testing

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function callSendAPI(sender_psid, response) {
         "message": response
     }
     request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "uri": "https://graph.facebook.com/v7.0/me/messages",
         "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
