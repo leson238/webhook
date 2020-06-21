370d55ef9b7270364df03bea0f3ee176f3a527d7
leson238 <leson238@gmail.com>
2020-06-20 22:22:40 -0400

    testing

--- index.js
+++ index.js
@@ -23,13 +23,12 @@ app.get("/webhook", (req, res) => {
     let token = req.query["hub.verify_token"]
     let challenge = req.query["hub.challenge"]
 
-    if (mode && token) {
-        if (mode === "subscribe" && token == VERIFY_TOKEN) {
+    if (token) {
+        if (token == VERIFY_TOKEN) {
             console.log("Webhook verified")
-            res.status(200).send(challenge)
+            res.status(200).send("challenge")
         } else {
             res.sendStatus(403)
         }
     }
-    res.send(req)
 })
