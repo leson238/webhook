b629e67d06e726ef29027bebf586b6903d0064a9
leson238 <leson238@gmail.com>
2020-06-21 03:25:27 -0400

    testing

--- index.js
+++ index.js
@@ -49,13 +49,52 @@ function handleMessage(sender_psid, received_message) {
         response = {
             "text": `You send the message: "${received_message.text}". Now send me an image!`
         }
+    } else if (received_message.attachments) {
+        let attachments_url = received_message.attachments[0].payload.url
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+          }
     }
     callSendAPI(sender_psid, response)
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-
+    let response;
+  
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+  
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
