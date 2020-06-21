2984fba187383cc81d7231653e647ca4b1208fcb
leson238 <leson238@gmail.com>
2020-06-21 03:19:19 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express().use(bodyParser.json())
+const request = require('request')
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
 app.listen(process.env.PORT || 1337, () => console.log("Webhook is listening"))
 app.post("/webhook", (req,res) => {
@@ -9,6 +11,13 @@ app.post("/webhook", (req,res) => {
         body.entry.forEach( entry => {
             let webhook_event = entry.messaging[0]
             console.log(webhook_event)
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid)
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message)
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback)
+            }
         });
         res.status(200).send("Event received")
     } else {
@@ -32,3 +41,42 @@ app.get("/webhook", (req, res) => {
         }
     }
 })
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+    if (received_message.text) {
+        response = {
+            "text": `You send the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+    callSendAPI(sender_psid, response)
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+    }
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
+      
+}
